2f5b5b97484464e10914a59ed85c5b5feb173cb1
vapor <vapor@uchicago.edu>
2025-03-28 20:46:19 -0500

    update readme

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/arc-modern/'  // Replace with your repository name
+    ? '/arc-modern/'  // Repository name on water-vapor's GitHub
     : '/',
   outputDir: 'dist',
   assetsDir: '',
